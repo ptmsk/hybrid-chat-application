d320031f99f13eb0fc9e808985200991677ca49a
Nguyễn Mậu Đức <nguyenmauduc91@gmail.com>
2022-11-26 21:52:22 +0700

    duc: fix UI for list user

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ socket.on("DANH_SACH_ONLINE", (userOnline) => {
   userOnline.forEach((user) => {
     const { username, peerId } = user;
     $("#ulUser").append(
-      `<li id="${peerId}" class="btn btn-secondary mb-2">${username}</li><br>`
+      `<li id="${peerId}" class="btn btn-secondary list-group-item mb-2">${username}</li>`
     );
   });
 
@@ -19,7 +19,7 @@ socket.on("DANH_SACH_ONLINE", (userOnline) => {
     const { username, peerId } = user;
     userOnline.push(user);
     $("#ulUser").append(
-      `<li id="${peerId}" class="btn btn-secondary mb-2">${username}</li><br>`
+      `<li id="${peerId}" class="btn btn-secondary list-group-item mb-2">${username}</li>`
     );
   });
 
